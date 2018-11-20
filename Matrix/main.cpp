91a8639aafbea2bff5c93a3ce373f8f2c8c8285b
Tobiasz Filar <tobiasz@MacBook-Pro-Tobiasz.local>
2018-11-20 15:31:28 +0100

    Initial Commit

---
+++ Matrix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrix
+//
+//  Created by Tobiasz Filar on 20/11/2018.
+//  Copyright © 2018 Tobiasz Filar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
